b3ec7f0d6a99dfd2653832fb1e0bac30ca7e0809
Christian Larsen <christianl@Christians-MBP.lan>
2023-01-15 20:09:29 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
---
+++ webshop/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'uk1d7se2',
+    dataset: 'production'
+  }
+})
